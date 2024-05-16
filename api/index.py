bdac087d40180a73b873eabacf3cde47bb4bb1d4
amoooooo <amor.budiyanto2006@gmail.com>
2024-05-16 23:52:06 +1200

    chat app v1

--- api/index.py
+++ api/index.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
+MESSAGES = {}
+USERS = {}
+ 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
+    data = request.form.to_dict()
+    username, password = data.values()
+    
+    if username in USERS and USERS.get(username, None) != password:
+        return render_template("index.html",
+                               error="Incorrect password!")
+    USERS[username] = password
+    return render_template('chat.html',
+                           username=username)
+
+@socketio.on('message')
+def handle_message(message):
+    # MESSAGES.append({'username': message['username'], 'message': message['text']})
+    emit('message', {'username': message['username'], 
+                     'text': message['text']}, broadcast=True)
