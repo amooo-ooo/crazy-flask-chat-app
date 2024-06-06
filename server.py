40ee3af826148116f82c140be1e83de4e1685e46
MrDewmon69 <149539322+MrDewmon69@users.noreply.github.com>
2024-06-06 12:56:47 +1200

    fixed your error

--- server.py
+++ server.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
-import uuid
 
 app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app)
 
-MESSAGES = {}
+MESSAGES = []
 USERS = {}
- 
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -18,20 +17,20 @@ def index():
 def chat():
     data = request.form.to_dict()
     username, password = data.values()
+
+    if username in USERS and USERS.get(username, None) != password:
+        return render_template("index.html", error="Incorrect password!")
     
-    if username in USERS and USERS.get(uuid.uuid4().hex, None)["password"] != password:
-        return render_template("index.html",
-                               error="Incorrect password!")
-    
-    USERS[uuid.uuid4().hex] = { "username": username,
-                               "password": password }
-    return render_template('chat.html',
-                           username=username)
+    USERS[username] = password
+    return render_template('chat.html', username=username)
 
 @socketio.on('message')
 def handle_message(message):
-    # MESSAGES.append({'username': message['username'], 'message': message['text']})
-    user = USERS.get(message['uid'], False)
+    MESSAGES.append({'username': message['username'],'message': message['text']})
+    user = USERS.get(message['username'], False)
     if user:
-        emit('message', {'username': user, 
-                        'text': message['text']}, broadcast=True)
+        emit('message', {'username': user,
+                         'text': message['text']}, broadcast=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
