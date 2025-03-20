7f66f369e399d812f1b07e87994d9b6b20ef8ab8
amooo-ooo <amor.budiyanto2006@gmail.com>
2025-03-21 00:11:42 +1300

    feat: concise front-end with alpine.js and secure accounts via user secrets

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
+import uuid
+import json
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app)
 
 MESSAGES = []
-USERS = {}
+USERS = {} 
+SECRETS = {}
 
 @app.route('/')
 def index():
@@ -16,21 +20,32 @@ def index():
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
     data = request.form.to_dict()
-    username, password = data.values()
+    username, password = data.get('username'), data.get('password')
 
-    if username in USERS and USERS.get(username, None) != password:
-        return render_template("index.html", error="Incorrect password!")
-    
-    USERS[username] = password
-    return render_template('chat.html', username=username)
+    if not username or not password:
+        return render_template("index.html", error="Username and password are required!")
+
+    if username in USERS:
+        if USERS[username]['password'] != password:
+            return render_template("index.html", error="Incorrect password!")
+    else:
+        user_secret = str(uuid.uuid4())
+        USERS[username] = {'secret': user_secret, 'password': password}
+        SECRETS[user_secret] = username
+
+    return render_template(
+        'chat.html',
+        username=username,
+        secret=USERS[username]['secret'],
+        messages=json.dumps(MESSAGES)
+    )
 
 @socketio.on('message')
 def handle_message(message):
-    MESSAGES.append({'username': message['username'],'message': message['text']})
-    user = USERS.get(message['username'], False)
-    if user:
-        emit('message', {'username': user,
-                         'text': message['text']}, broadcast=True)
+    username = SECRETS.get(message['secret'])
+    if username:
+        MESSAGES.append({'username': username, 'message': message['text']})
+        emit('message', {'username': username, 'text': message['text']}, broadcast=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
